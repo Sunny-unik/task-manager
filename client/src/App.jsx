2390ae37ccf96d37f03739bd5c06ede7eda82d38
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-05-19 11:42:18 +0530

    feat: add Header component with client routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,41 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <h1 className="text-3xl font-bold max-w-7xl mx-auto">
+                Hello world!
+              </h1>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <h1 className="text-3xl font-bold max-w-7xl mx-auto">Profile</h1>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <h1 className="text-3xl font-bold max-w-7xl mx-auto">Login</h1>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <h1 className="text-3xl font-bold max-w-7xl mx-auto">Signup</h1>
+            }
+          />
+        </Routes>
+      </Router>
     </>
   );
 }
